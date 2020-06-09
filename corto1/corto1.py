9372d50b8b2f8201ee108063a2038e862fc064ec
usac201344551 <2353107940511@ingenieria.usac.edu.gt>
2020-06-09 12:29:54 -0600

    2da version

--- corto1/corto1.py
+++ corto1/corto1.py
@@ -3,21 +3,6 @@
 # número equivalente a los últimos 3 dígitos de su carnet. 
 #carnet= 201344551, final de rango=551
 #
-'''
-numero = int(input("Ingresa un numero: "))
-
-while numero != 1:
-    if numero % 2 == 0:
-        print("%d," % (numero), end = "")
-        numero = numero / 2
-    else:
-        print("%d," % (numero), end = "")
-        numero = (numero * 3) + 1
-
-    if numero == 1:
-        print("1")
-'''
-
 fin_carnet=551
 fin=fin_carnet+1
 savecollatz=[]
@@ -29,14 +14,26 @@ for i in range(2,6):
         if i % 2 == 0:
             print("%d," % (i), end = "")
             i = i / 2
-            #savecollatz.append(int(i))
+            savecollatz.append(int(i))
         else:
             print("%d," % (i), end = "")
             i = (i * 3) + 1
-            #savecollatz.append(int(i))
+            savecollatz.append(int(i))
         if i == 1:
             print("1")
         savecollatz.append(int(i))
-
-print(savecollatz)
+    print(savecollatz)
     
+#print(savecollatz)
+'''
+def fileAppend(fileName = 'collatz.txt'):
+    archivo = open(fileName,'a') #Abrir para agregar a archivo existente
+    archivo.write('\n\nNuevo evento de append...\n')
+    print('Espere, agregando datos al archivo...')
+    for i in range(6):
+        archivo.write(str(savecollatz)
+
+    archivo.close() #Siempre cerrar el archivo al finalizar la escritura
+    print('\n\nAppend finalizado')
+'''
+
